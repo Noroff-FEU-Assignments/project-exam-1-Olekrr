ccca1a87a940c80d5991f215518e89582cbbb465
Olekrr <93515165+Olekrr@users.noreply.github.com>
2023-05-28 16:32:23 +0200

    code formatting and minor tweaks

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,4 @@
+/*event listener for hamburger menu on mobile view*/
 document.querySelector('.hamburger-btn').addEventListener('click', function(){
     this.classList.toggle('active');
     document.querySelector('.menu-container').classList.toggle('active');
